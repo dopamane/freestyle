0f3e64f7b8956c16241e065df14fbe5dd7926435
dopamane <dwc1295@gmail.com>
2026-05-17 11:04:10 -0700

    update docs

--- README.md
+++ README.md
@@ -2,6 +2,29 @@
 
 Concurrent, pretty TUI
 
+Example
+```hs
+main :: IO ()
+main = do
+  f <- newFreestyleIO
+  s <- newTVarIO $ \"FREESTYLE!!!FREESTYLE!!!FREESTYLE!!!\"
+  concurrently_ (runFreestyle f $ cfg s) $
+    forever $ do
+      atomically $ modifyTVar' s rote
+      threadDelay 200000
+  where
+    rote s = last s : init s
+
+cfg :: TVar String -> FreestyleCfg String AnsiStyle
+cfg s = FreestyleCfg
+  { initState = readTVar s
+  , drawState = \\s' -> return $
+    applyWhen (take 1 s' == "E") (annotate $ color Blue) $ pretty s'
+  , layoutDoc = layoutPretty defaultLayoutOptions
+  , renderDoc = renderLazy
+  }
+```
+
 Development
 ```
 cabal build
--- test/Main.hs
+++ test/Main.hs
@@ -5,7 +5,6 @@ import Control.Concurrent.Async
 import Control.Concurrent.STM
 import Control.Monad
 import Data.Function
-import Data.Semigroup
 import Freestyle
 import Prettyprinter
 import Prettyprinter.Render.Terminal
@@ -13,13 +12,13 @@ import Prettyprinter.Render.Terminal
 main :: IO ()
 main = do
   f <- newFreestyleIO
-  s <- newTVarIO $ stimes (4 :: Int) "FREESTYLE|"
+  s <- newTVarIO $ "FREESTYLE!!!FREESTYLE!!!FREESTYLE!!!"
   concurrently_ (runFreestyle f $ cfg s) $
     forever $ do
       atomically $ modifyTVar' s rote
       threadDelay 200000
   where
-    rote s = last s : init s -- drop 1 s <> take 1 s
+    rote s = last s : init s
 
 cfg :: TVar String -> FreestyleCfg String AnsiStyle
 cfg s = FreestyleCfg

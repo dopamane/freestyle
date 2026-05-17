56ec781993c8f9ac42aaa16303ff2543cd002e3e
dopamane <dwc1295@gmail.com>
2026-05-17 11:24:57 -0700

    add haddock docs

--- lib/Freestyle/Freestyle.hs
+++ lib/Freestyle/Freestyle.hs
@@ -32,12 +32,13 @@ newFreestyle =
 
 -- | User init configuration
 data FreestyleCfg s ann = FreestyleCfg
-  { initState :: STM s
-  , drawState :: s -> STM (Doc ann)
-  , layoutDoc :: Doc ann -> SimpleDocStream ann
-  , renderDoc :: SimpleDocStream ann -> Text
+  { initState :: STM s                          -- ^ read the current state
+  , drawState :: s -> STM (Doc ann)             -- ^ draw the current state
+  , layoutDoc :: Doc ann -> SimpleDocStream ann -- ^ layout the doc
+  , renderDoc :: SimpleDocStream ann -> Text    -- ^ render the doc
   }
 
+-- | Run the TUI with the configuration
 runFreestyle :: Eq s => FreestyleCfg s ann -> IO a
 runFreestyle cfg = join $ atomically $ do
   f <- newFreestyle
@@ -52,6 +53,7 @@ initFreestyle f cfg = do
   setState  f $ initState cfg
   setDraw   f $ drawState cfg
 
+-- | Display the current state then wait a change to re-display
 runState :: Eq s => Freestyle s ann -> IO a
 runState f = forever $ join $ atomically $ do
   s <- join $ readTMVar (stateVar f)
@@ -61,14 +63,18 @@ runState f = forever $ join $ atomically $ do
     s' <- join $ readTMVar (stateVar f)
     check $ s /= s'
 
+-- | Change the layout algorithm
 setLayout :: Freestyle s ann -> (Doc ann -> SimpleDocStream ann) -> STM ()
 setLayout f = D.setLayout $ display f
 
+-- | Change the rendering algorithm
 setRender :: Freestyle s ann -> (SimpleDocStream ann -> Text) -> STM ()
 setRender f = D.setRender $ display f
 
+-- | Change the state accesor
 setState :: Freestyle s ann -> STM s -> STM ()
 setState f = writeTMVar $ stateVar f
 
+-- | Change the drawing algorithm
 setDraw :: Freestyle s ann -> (s -> STM (Doc ann)) -> STM ()
 setDraw f = writeTMVar $ drawVar f

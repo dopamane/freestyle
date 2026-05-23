c32d38dbf730ae5f08d1105759cc81d20fced277
dopamane <dwc1295@gmail.com>
2026-05-22 21:50:43 -0700

    cleanup

--- test/Main.hs
+++ test/Main.hs
@@ -152,25 +152,3 @@ data Style
   | Title
   | Border
   deriving (Eq, Show)
-
-{-
-main :: IO ()
-main = do
-  s <- newTVarIO "FREESTYLE!!!FREESTYLE!!!FREESTYLE!!!"
-  f <- newFreestyleIO
-  concurrently_ (runFreestyle f $ cfg s) $
-    forever $ do
-      atomically $ modifyTVar' s rote
-      threadDelay 200000
-  where
-    rote s = last s : init s
-
-cfg :: TVar String -> FreestyleCfg String AnsiStyle
-cfg s = FreestyleCfg
-  { readState = readTVar s
-  , drawState = \s' -> return $
-    applyWhen (take 1 s' == "E") (annotate $ color Blue) $ pretty s'
-  , layoutDoc = return . layoutPretty defaultLayoutOptions
-  , renderDoc = return . renderLazy
-  }
--}

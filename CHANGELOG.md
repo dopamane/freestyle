1262a12143b6967b2308f55522e3f1fc050057a3
dopamane <dwc1295@gmail.com>
2026-05-24 02:21:22 -0700

    simplify

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,5 @@
 # Revision history for renderboy
 
-## 0.1.0.0 -- YYYY-mm-dd
+## 0.1.0.0 -- 5-dd-2026
 
 * First version. Released on an unsuspecting world.
--- lib/Freestyle.hs
+++ lib/Freestyle.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 -- | Concurrent, pretty TUI
 --
 -- @
@@ -35,4 +37,142 @@ module Freestyle
   , setDraw
   ) where
 
-import Freestyle.Freestyle
+import Control.Concurrent.STM
+import Control.Exception
+import Control.Monad
+import Data.Text.Lazy (Text)
+import qualified Data.Text.Lazy as T
+import Data.Text.Lazy.Builder
+import qualified Data.Text.Lazy.IO as TIO
+import Prettyprinter
+import System.IO
+
+-- | TUI handle
+data Freestyle s ann = Freestyle
+  { lay      :: TMVar (Doc ann -> STM (SimpleDocStream ann))
+  , ren      :: TMVar (SimpleDocStream ann -> STM Text)
+  , stateVar :: TMVar (STM s)              -- current state
+  , drawVar  :: TMVar (s -> STM (Doc ann)) -- draw state
+  }
+
+-- | Construct a new STM TUI
+newFreestyle :: STM (Freestyle s ann)
+newFreestyle =
+  Freestyle
+    <$> newEmptyTMVar
+    <*> newEmptyTMVar
+    <*> newEmptyTMVar
+    <*> newEmptyTMVar
+
+-- | Construct a new IO TUI
+newFreestyleIO :: IO (Freestyle s ann)
+newFreestyleIO = atomically newFreestyle
+
+-- | User init configuration.
+-- The TUI updates when current state changes.
+data FreestyleCfg s ann = FreestyleCfg
+  { readState :: STM s                                -- ^ read the current state
+  , drawState :: s -> STM (Doc ann)                   -- ^ draw the current state
+  , layoutDoc :: Doc ann -> STM (SimpleDocStream ann) -- ^ layout the doc
+  , renderDoc :: SimpleDocStream ann -> STM Text      -- ^ render the doc
+  }
+
+-- | Run the TUI with the configuration
+runFreestyle :: Eq s => Freestyle s ann -> FreestyleCfg s ann -> IO a
+runFreestyle f cfg = do
+  atomically $ initFreestyle f cfg
+  runState f
+
+-- | Initialize required configurations
+initFreestyle :: Freestyle s ann -> FreestyleCfg s ann -> STM ()
+initFreestyle f cfg = do
+  setLayout f $ layoutDoc cfg
+  setRender f $ renderDoc cfg
+  setState  f $ readState cfg
+  setDraw   f $ drawState cfg
+
+-- | Display the current state then wait a change to re-display
+runState :: Eq s => Freestyle s ann -> IO a
+runState f = withTerm $ loop Nothing `finally` output (eraseScreen <> home)
+  where
+    loop Nothing = join $ atomically $ do
+      s <- join $ readTMVar (stateVar f)
+      t <- renderText f s
+      return $ do
+        output $ eraseScreen <> home <> t
+        loop $ Just (s, t)
+    loop (Just (s, p)) = join $ atomically $ do
+      s' <- join $ readTMVar (stateVar f)
+      check $ s' /= s
+      t <- renderText f s'
+      return $ do
+        output $ toLazyText $ composite t p
+        loop $ Just (s', t)
+
+renderText :: Freestyle s ann -> s -> STM Text
+renderText f s = do
+  draw <- readTMVar $ drawVar f
+  layo <- readTMVar $ lay f
+  rend <- readTMVar $ ren f
+  rend =<< layo =<< draw s
+
+-- | Change the layout algorithm
+setLayout :: Freestyle s ann -> (Doc ann -> STM (SimpleDocStream ann)) -> STM ()
+setLayout f = writeTMVar $ lay f
+
+-- | Change the rendering algorithm
+setRender :: Freestyle s ann -> (SimpleDocStream ann -> STM Text) -> STM ()
+setRender f = writeTMVar $ ren f
+
+-- | Change the state reader
+setState :: Freestyle s ann -> STM s -> STM ()
+setState f = writeTMVar $ stateVar f
+
+-- | Change the drawing algorithm
+setDraw :: Freestyle s ann -> (s -> STM (Doc ann)) -> STM ()
+setDraw f = writeTMVar $ drawVar f
+
+-- | write stdout and flush
+output :: Text -> IO ()
+output t = TIO.putStr t >> hFlush stdout
+
+home :: Text
+home = "\x1b[H"
+
+eraseLine :: Builder
+eraseLine = "\x1b[0K"
+
+eraseScreen :: Text
+eraseScreen = "\x1b[2J"
+
+movRow :: Int -> Builder
+movRow n = "\x1b[" <> fromString (show (n + 1)) <> "H"
+
+composite :: Text -> Text -> Builder
+composite new old = go 0 (T.lines new) (T.lines old)
+  where
+    go _ [] [] = mempty
+    go _ ns [] = fromLazyText $ T.unlines ns
+    go _ [] (_:_) = "\x1b[J"
+    go r (n:ns) (o:os) = mconcat
+      [ if n /= o then movRow r <> fromLazyText n else mempty
+      , if T.length o > T.length n then eraseLine else mempty
+      , go (r + 1) ns os
+      ]
+
+withTerm :: IO a -> IO a
+withTerm k = withoutEcho $ do
+  hSetBuffering stdout $ BlockBuffering Nothing
+  withoutCursor k
+
+withoutCursor :: IO a -> IO a
+withoutCursor =
+  bracket_
+    (output "\x1b[?25l") -- hide cursor
+    (output "\x1b[?25h") -- show cursor
+
+withoutEcho :: IO a -> IO a
+withoutEcho =
+  bracket_
+    (hSetEcho stdin False)
+    (hSetEcho stdin True)
